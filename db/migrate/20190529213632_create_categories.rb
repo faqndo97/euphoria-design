9ebfb1274c93b914437464b4a3274f7d75af030d
facundo espinosa <facundo.espinosa@fing.edu.uy>
2019-05-29 18:45:51 -0300

    add category

---
+++ db/migrate/20190529213632_create_categories.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+# rubocop:disable Style/Documentation
+class CreateCategories < ActiveRecord::Migration[6.0]
+  def change
+    create_table :categories do |t|
+      t.string :name
+    end
+  end
+end
+# rubocop:enable Style/Documentation
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_05_28_005015) do
+ActiveRecord::Schema.define(version: 2019_05_29_213632) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -44,6 +44,10 @@ ActiveRecord::Schema.define(version: 2019_05_28_005015) do
     t.string "checksum", null: false
     t.datetime "created_at", null: false
     t.index ["key"], name: "index_active_storage_blobs_on_key", unique: true
+  end
+
+  create_table "categories", force: :cascade do |t|
+    t.string "name"
   end
 
   create_table "posts", force: :cascade do |t|
