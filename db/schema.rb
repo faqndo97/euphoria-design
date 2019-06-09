878bb632078b9cddac51ade7c8057f2cd3609e64
facundo espinosa <facundo.espinosa@fing.edu.uy>
2019-06-09 16:54:03 -0300

    add description to posts

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_05_29_224635) do
+ActiveRecord::Schema.define(version: 2019_06_09_193844) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -63,6 +63,7 @@ ActiveRecord::Schema.define(version: 2019_05_29_224635) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.integer "state"
+    t.text "description"
     t.index ["title"], name: "index_posts_on_title"
   end
 
