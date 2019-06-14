edefc3bb41afc7c72467d80dc41fb2234936acf1
facundo espinosa <facundo.espinosa@fing.edu.uy>
2019-06-13 22:39:32 -0300

    add title lenght validation on post model

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 class Post < ApplicationRecord
   has_rich_text :body
   has_one_attached :cover
@@ -8,4 +10,5 @@ class Post < ApplicationRecord
 
   validates :title, :description, :state, presence: true
   validates :description, length: { maximum: 280 }
+  validates :title, length: { maximum: 140 }
 end
