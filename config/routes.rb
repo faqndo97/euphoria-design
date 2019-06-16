90d281be1738483d76c3e644c4e68c393861173c
facundo espinosa <facundo.espinosa@fing.edu.uy>
2019-06-16 17:53:15 -0300

    add devise controllers, views, customize devise admin routes, set layout to devise session and design sign in to admin users

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,18 @@
+# frozen_string_literal: true
+
 Rails.application.routes.draw do
-  devise_for :admin_users
   root to: 'posts#index'
 
   resources :posts, only: %i[index show]
 
   namespace :admin do
-    resources :posts
-    resources :categories
+    devise_for :admin_users, path: 'auth', controllers: {
+      sessions: 'admin_users/sessions'
+    }
 
     root to: 'posts#index'
+
+    resources :posts
+    resources :categories
   end
 end
